07036c117a32a9361c7068b92342ebccf89e671d
saloni vashishth <132837742+OIBSIP1@users.noreply.github.com>
2025-03-17 22:35:31 +0530

    Delete Project 295 directory

--- Project 295/test1.py
+++
@@ -1,9 +0,0 @@
-#Regular Expression
-import re
-k="The rain in Spain"
-
-x=re.search("^The.*Spain$",k)   #.* will tell whether words are there in btw or not
-if(x):
-    print("yes")
-else:
-    print("No Match")
